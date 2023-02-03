bf855538d10998d6a932deaba12af16733316da2
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2023-02-03 17:00:38 +0530

    :construction: wip

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletDialogProvider } from "@solana/wallet-adapter-material-ui";
+import { PhantomWalletAdapter, GlowWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { useToast } from "@chakra-ui/react";
+import { clusterApiUrl } from "@solana/web3.js";
+import { useCallback, useEffect, useState } from "react";
+import { web3 } from "@project-serum/anchor";
+import Main from "./components/Main";
+require('@solana/wallet-adapter-react-ui/styles.css');
 
-function App() {
+const devnet = clusterApiUrl("devnet");
+const network = devnet;
+
+
+function AppWrappedWithProviders() {
+  const [voteAccount, setVoteAccount] = useState(null);
+
+  const toast = useToast();
+
+  const wallets = useMemo(
+        () => [
+            new PhantomWalletAdapter(),
+            new GlowWalletAdapter(),
+        ],
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [network]
+    );
+
+  useEffect(() => {
+    fetch("/voteAccount")
+      .then((response) => response.json())
+      .then((data) => {
+        const accountArray = Object.values(data.voteAccount._keypair.secretKey);
+        const secret = new Uint8Array(accountArray);
+        const kp = web3.Keypair.fromSecretKey(secret);
+        setVoteAccount(kp);
+      })
+      .catch((error) => {
+        console.log(error);
+        enqueueSnackbar("Could not fetch vote account", { variant: "error" });
+      });
+  }, [enqueueSnackbar]);
+
+  const onWalletError = useCallback(
+    (error) => {
+      enqueueSnackbar(
+        error.message ? `${error.name}: ${error.message}` : error.name,
+        { variant: "error" }
+      );
+      console.error(error);
+    },
+    [enqueueSnackbar]
+  );
+
+  // Wrap <Main /> within <WalletProvider /> so that we can access useWallet hook within Main
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WalletProvider wallets={wallets} onError={onWalletError} autoConnect>
+      <WalletModalProvider>
+        <Main network={network} voteAccount={voteAccount} />
+      </WalletModalProvider>
+    </WalletProvider>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+        <ConnectionProvider endpoint={network}>
+          <AppWrappedWithProviders />
+        </ConnectionProvider>
+  );
+}
