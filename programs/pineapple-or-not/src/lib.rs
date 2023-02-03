ecd4a76c554a4525f80e4b334db5c24f7d3b555a
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2023-02-03 10:42:46 +0530

    :rocket: update program id

--- programs/pineapple-or-not/src/lib.rs
+++ programs/pineapple-or-not/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("4cnMefx5UmwwW6yCJYMoLAGzQy8XVq7AQRKqFsP7aajh");
 
 #[program]
 pub mod pineapple_or_not {
